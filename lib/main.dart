b23bc0a90b5695652f7fdfc2bd6aed8dc1257ca7
Ascrion <navodiths@gmail.com>
2025-06-19 23:06:19 +0530

    Save after completing screen layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'screens/home_screen.dart';
+import 'widgets/theme_data.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Exposure Explorers Reshot',
+      debugShowCheckedModeBanner: false,
+
+      // Color theme
+      theme: customTheme,
+
+      // Routing
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+      },
     );
   }
 }
---
+++ lib/widgets/theme_data.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+// Custom Theme: Custom colors and text
+final ThemeData customTheme = ThemeData(
+
+  colorScheme: ColorScheme(
+    primary: Color(0xFF0C0C0C),
+    onPrimary: Colors.white70, // content such as text sitting on top of the primary color
+    brightness: Brightness.dark,
+    secondary: Color(0xFFFF7A00),
+    secondaryFixed: Color.fromARGB(255, 123, 60, 0),
+    secondaryFixedDim: Color.fromARGB(255, 65, 34, 6),
+    onSecondary: Colors.white,
+    tertiary: Color(0xFF2E2E2E), // Subtle surface circle
+    onTertiary: Colors.white,
+    surface: Color.fromARGB(235, 29, 29, 29), //  Background of cards and menus that sit on top of app background
+    onSurface: Color.fromARGB(255, 22, 22, 22),
+    surfaceContainerHighest: Color(0xFF1A1A1A), //	Highest-level surface layer (used in Material 3 elevations).
+    onSurfaceVariant: Color(0xFFB0B0B0), //Variant for nested or less prominent surfaces (dividers)
+    error: Colors.red,
+    onError: Colors.white,
+    outline: Colors.orangeAccent,
+  ),
+
+textTheme: TextTheme(
+  headlineLarge: GoogleFonts.playfairDisplay(
+    fontSize: 32,
+    fontWeight: FontWeight.w700, // Bold
+    letterSpacing: 1.2,
+  ),
+  labelLarge: GoogleFonts.lora(
+    fontSize: 25,
+    fontWeight: FontWeight.w400, // Regular
+    height: 1.5,
+  ),
+  labelMedium: GoogleFonts.lora(
+    fontSize: 18,
+    fontWeight: FontWeight.w400, // Regular
+    height: 1.5,
+  ),
+  bodyMedium: GoogleFonts.lora(
+    fontSize: 16,
+    fontWeight: FontWeight.w400, // Regular
+    height: 1.5,
+  ),
+),
+
+);
